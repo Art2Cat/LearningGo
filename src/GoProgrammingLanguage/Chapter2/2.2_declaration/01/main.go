8b360c69fe2d5f35adeedf9e50efe55ccd161186
art2cat <yiming.whz@gmail.com>
2016-12-26 19:55:16 +0800

    update

--- src/GoProgrammingLanguage/Chapter2/2.2_declaration/01/main.go
+++ src/GoProgrammingLanguage/Chapter2/2.2_declaration/01/main.go
@@ -1,11 +1,11 @@
 package main
 
 import "fmt"
-//常量boilingF是在包一级范围声明语句声明的，可在整个包一级范围内访问
+
+//全局(包范围内)常量沸点Fahrenheit
 const boilingF = 212.0
 
 func main() {
-	//f和c两个变量是在main函数内部声明的声明语句声明的，只能在函数内部访问
 	var f = boilingF
 	var c = (f - 32) * 5 / 9
 	fmt.Printf("boiling point = %g°F or %g°C\n", f, c)
--- src/GoProgrammingLanguage/Chapter2/2.2_declaration/02/main.go
+++ src/GoProgrammingLanguage/Chapter2/2.2_declaration/02/main.go
@@ -1,15 +1,13 @@
-// Ftoc prints two Fahrenheit-to-Celsius conversions.
 package main
 
 import "fmt"
 
 func main() {
-	//这里使用在main函数中定义的两个常量作为被调用fToC函数的参数
 	const freezingF, boilingF = 32.0, 212.0
 	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF)) // "32°F = 0°C"
 	fmt.Printf("%g°F = %g°C\n", boilingF, fToC(boilingF))   // "212°F = 100°C"
 }
-//这里函数fToC的f参数是float64类型，函数运行后返回结果为float64类型的值
+
 func fToC(f float64) float64 {
 	return (f - 32) * 5 / 9
 }
